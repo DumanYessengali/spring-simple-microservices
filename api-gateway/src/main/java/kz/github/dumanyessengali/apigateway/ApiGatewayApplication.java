2944e1bf215e477bdd1253dc9ee796d11bed84c3
DumanYessengali <balmuaast@gmail.com>
2022-07-30 14:39:57 +0600

    add api gateway to microservice

---
+++ api-gateway/src/main/java/kz/github/dumanyessengali/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package kz.github.dumanyessengali.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+}
